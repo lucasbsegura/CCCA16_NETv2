c64c950b9d0b99336a8e9de4314694e517cd65be
Lucas Segura <lucasbsegura@gmail.com>
2024-05-23 22:35:54 -0300

    Adding Test Projects

---
+++ CCCA16_NETv2.PaymentApp.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CCCA16_NETv2.PaymentApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CCCA16_NETv2.RideApp.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CCCA16_NETv2.RideApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
